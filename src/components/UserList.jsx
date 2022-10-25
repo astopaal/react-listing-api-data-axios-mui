a87bf27e144fc8f1935ae305a6f896048a597a0f
abdussamet topal <85809119+astopaal@users.noreply.github.com>
2022-10-25 19:49:13 +0300

    alert func changed to mui dialog. you can show clicked rows user data in dialog

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -9,84 +9,118 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import "../styles/UserList.css";
-import UserDetail from "./UserDetail";
+import {Button, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText} from "@mui/material";
 
 //https://randomuser.me/api/?results=10
 
 const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const fetchUsers = async () => {
-    const response = await axios.get("https://randomuser.me/api/?results=10");
-    setUsers(response.data.results);
-  };
+    const [users, setUsers] = useState([]);
+    const [showUser, setShowUser] = useState(false);
+    const [userData, setUserData] = useState({});
+    const fetchUsers = async () => {
+        const response = await axios.get("https://randomuser.me/api/?results=10");
+        setUsers(response.data.results);
+    };
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-  return (
-    <div className="container">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className="headCell">First Name</TableCell>
-              <TableCell className="headCell" align="right">
-                Last Name
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                Gender
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                Age
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                Country
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                State
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                City
-              </TableCell>
-              <TableCell className="headCell" align="right">
-                Email
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow
-                key={user.login.uuid}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                onClick={() => {
-                  alert(
-                    "Name : " + user.name.first + " " + user.name.last + "\n" + 
-                    "Gender : " + user.gender + "\n" + "Age : " + user.dob.age + "\n" + 
-                    "Phone : " + user.phone + "\n" + "Cell : " + user.cell + "\n" +
-                    "Country : " + user.location.country + "\n" + "State : " + user.location.state + "\n" +
-                    "City : " + user.location.city + "\n" + "Email : " + user.email + "\n" +
-                    "Picture : " + user.picture.large
-                  );
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  {user.name.first}
-                </TableCell>
-                <TableCell align="right">{user.name.last}</TableCell>
-                <TableCell align="right">{user.gender}</TableCell>
-                <TableCell align="right">{user.dob.age}</TableCell>
-                <TableCell align="right">{user.location.country}</TableCell>
-                <TableCell align="right">{user.location.state}</TableCell>
-                <TableCell align="right">{user.location.city}</TableCell>
-                <TableCell align="right">{user.email}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+    return (
+        <div className="container">
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className="headCell">First Name</TableCell>
+                            <TableCell className="headCell" align="right">
+                                Last Name
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                Gender
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                Age
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                Country
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                State
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                City
+                            </TableCell>
+                            <TableCell className="headCell" align="right">
+                                Email
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((user) => (
+                            <TableRow
+                                key={user.login.uuid}
+                                sx={{"&:last-child td, &:last-child th": {border: 0}}}
+                                onClick={() => {
+                                    setShowUser(true);
+                                    setUserData(user);
+                                }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {user.name.first}
+                                </TableCell>
+                                <TableCell align="right">{user.name.last}</TableCell>
+                                <TableCell align="right">{user.gender}</TableCell>
+                                <TableCell align="right">{user.dob.age}</TableCell>
+                                <TableCell align="right">{user.location.country}</TableCell>
+                                <TableCell align="right">{user.location.state}</TableCell>
+                                <TableCell align="right">{user.location.city}</TableCell>
+                                <TableCell align="right">{user.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Dialog className = 'detail-dialog' open={showUser} onClose={() => setShowUser(false)}>
+                <DialogTitle>User Details</DialogTitle>
+                <DialogContent className = 'detail-dialog-content'>
+                    <DialogContentText>
+                        <img src={userData.picture?.large}/>
+                    </DialogContentText>
+                    <DialogContentText>
+                        First Name: {userData.name?.first}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Last Name: {userData.name?.last}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Age : {userData.dob?.age}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Gender : {userData.gender}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Location : {userData.location?.city}, {userData.location?.state}, {userData.location?.country}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Email: {userData.email}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Phone: {userData.phone}
+                    </DialogContentText>
+                    <DialogContentText>
+                        Cell: {userData.cell}
+                    </DialogContentText>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setShowUser(false)}>Close</Button>
+                </DialogActions>
+            </Dialog>
+
+
+        </div>
+    );
 };
 
 export default UserList;
